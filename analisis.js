e8e3c8e669b762985952a11b8b1dc9ed4dc80c35
Francisco Escobar <franciscojescobar@gmail.com>
2022-04-12 09:42:37 -0500

    Encapsulando analisis.js en funciones y entendiendo el método splice

--- analisis.js
+++ analisis.js
@@ -1,4 +1,4 @@
-//helpers
+//funcioines helpers o utils
 function esPar(num)
 {
     return num % 2 === 0;   
@@ -19,64 +19,99 @@ function calcularMediaAritmetica(array)
     return mediaArray;    
 }
 
+/******************************************************************************************* */
 
-//hallar la mediana del array de objetos salariosColombia
+//hallar la mediana del array de objetos listaSalarios
 //pero primero debemos ordenar el array
 
-const salariosCol = salariosColombia.map
-(
-    function (elemento)
-    {
-        return elemento.salario;
-    }
-
-);
-
-const salariosColOrdenados = salariosCol.sort
-(
-    function (salarioA, SalarioB)
-    {
-        return salarioA - SalarioB;
-    }
-); 
-
-let mediana;
-
-const mitad = parseInt(salariosColOrdenados.length / 2);
-
-
-if (esPar(salariosColOrdenados.length)) //ES LENGTH   TH AL FINAL!!!!!
+function ordenarListaSalarios(listaSalarios)
 {
-    const mitad1 = mitad - 1;
-    const mitad2 = mitad;
-    const arrayDosMitades = [ salariosColOrdenados[mitad1], salariosColOrdenados[mitad2] ];
-    console.log(arrayDosMitades);
-    mediana = calcularMediaAritmetica(arrayDosMitades);    
+    const salariosPais = listaSalarios.map
+    (
+        function (elemento)
+        {
+            return elemento.salario;
+        }
+    );
+
+    const salariosPaisOrdenados = salariosPais.sort
+    (
+        function (salarioA, SalarioB)
+        {
+            return salarioA - SalarioB;
+        }
+    ); 
+
+    return salariosPaisOrdenados;
 }
 
-else
-{
-    mediana = salariosColOrdenados[mitad]; 
-}
 
-console.log("Salarios Ordenados de menor a mayor a continuación")
-console.log(salariosColOrdenados);
-console.log("La MEDIANA de los salarios de toda Colombia es " + mediana);
+
+function medianaSalarios (listaSalarios)
+{ 
+    let mediana;    
+    const salariosPaisOrdenados = ordenarListaSalarios(listaSalarios);
+    const mitad = parseInt(salariosPaisOrdenados.length / 2);
+
+    if (esPar(salariosPaisOrdenados.length)) //ES LENGTH   TH AL FINAL!!!!!
+    {
+        const salarioPersonaMitad1 = salariosPaisOrdenados[mitad - 1];
+        const salarioPersonaMitad2 = salariosPaisOrdenados[mitad];       
+        mediana = calcularMediaAritmetica([salarioPersonaMitad1, salarioPersonaMitad2]);    
+    }
+    else
+    {
+        mediana = salariosPaisOrdenados[mitad]; 
+    }    
+    console.log("Salarios Ordenados de menor a mayor a continuación")
+    console.log(salariosPaisOrdenados);
+    console.log("La MEDIANA de los salarios de toda Colombia es: ");
+    
+    return mediana;
+
+}
 
 // ahora hallaré la media aritmética del top 10% de salarios de Colombia
-const porcentajeExcluido = 0.90;
-const top10Indice = parseInt(salariosColOrdenados.length * porcentajeExcluido);
 
-const salariosColTop10 = salariosColOrdenados.splice(top10Indice, salariosColOrdenados.length - 1);
-console.log("El 10% de salarios más altos de Colombiaa continuación");
-console.log(salariosColTop10);
 
-const mediaAritmeticaTop10 = calcularMediaAritmetica(salariosColTop10);
-console.log(`La media aritmética del top ${Math.ceil((1-porcentajeExcluido)*100)}
-% es ${mediaAritmeticaTop10}`);
+function mediaAritmeticaSalariosTop10(listaSalarios)
+{
+    const salariosPaisOrdenados = ordenarListaSalarios(listaSalarios);
 
-//OJO VER BIEN QUE HACE EL METODO SPLICE SEGUN EXPLICA EL PROFE JUAN 
-//PORQUE ME FUNCIONA PERO NO LO ENTIENDO A MUY BIEN QUE DIGAMOS
+    const salariosPaisTop10 = salariosPaisOrdenados.splice(top10Indice);
+
+    const porcentajeExcluido = 0.90;
+    const top10Indice = parseInt(salariosPaisOrdenados.length * porcentajeExcluido);
+
+    
+    console.log("El 10% de salarios más altos de Colombiaa continuación");
+    console.log(salariosPaisTop10);
+
+    const mediaAritmeticaTop10 = calcularMediaAritmetica(salariosPaisTop10);
+
+    return `La media aritmética del top ${Math.ceil((1-porcentajeExcluido)*100)}% es ${mediaAritmeticaTop10}`;
+
+    //OJO VER BIEN QUE HACE EL METODO SPLICE SEGUN EXPLICA EL PROFE JUAN 
+    //PORQUE ME FUNCIONA PERO NO LO ENTIENDO MUY BIEN QUE DIGAMOS
+}
+
+
+
+function pruebaSlice()
+{    
+    const arrayEjemplo = [0,1,2,3,4,"Francisco 5","Sandra 6",7,8,9,10];
+    const spliceEjemplo = arrayEjemplo.splice(5,2);
+
+    console.log
+    (
+        {arrayEjemplo},
+        {spliceEjemplo}
+
+    );
+}
+
+
+
 
 
 
