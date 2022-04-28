d2458e5dfa0ad110ccc402b0028f626cb77628f2
Francisco Escobar <franciscojescobar@gmail.com>
2022-04-28 16:08:34 -0500

    Creé carpeta aparte para presentar el crédito francés

--- credito-frances/credito-frances.js
+++ credito-frances/credito-frances.js
@@ -47,24 +47,6 @@ function calcularCuota() {
     return objetoAmortizacion;
 }
 
-    /*        
-    CÓDIGO DE COMO CREAR UN NUEVO TAG E INSERTARLO DEBAJO DE OTRO NUEVO
-    for (let i = 1; i <= cantidadPeriodos; i++)
-        const nuevoParrafo = document.createElement("p");
-        nuevoParrafo.innerText = i;  
-        document.body.appendChild(nuevoParrafo);  
-    }
-     
-    OTRO CÓDIGO. EM EL QUE ME BASÉ
-    for(i=1;i<=num_stud;i++)
-    {
-        newDiv = document.createElement('div');
-        divIdName = '50'+i;
-        newdiv.setAttribute('id',divIdName);
-        newdiv.innerHTML ='<div id="box'+i+'">Testing 123</div>';
-        document.body.appendChild(newdiv);
-    }
-    */
 
 function onClickAmortizacion()
 {
---
+++ credito-frances/utils.js
@@ -0,0 +1,58 @@
+function mesTextual(mesDePago)
+{
+    let mesDePagoTextual;
+
+    switch (mesDePago)
+    {
+        case 1:
+            mesDePagoTextual = "Enero";
+        break;
+        case 2:
+            mesDePagoTextual = "Febrero";
+        break;
+        case 3:
+            mesDePagoTextual = "Marzo";
+        break;
+        case 4:
+            mesDePagoTextual = "Abril";
+        break;
+        case 5:
+            mesDePagoTextual = "Mayo";
+        break;
+        case 6:
+            mesDePagoTextual = "Junio";
+        break;
+        case 7:
+            mesDePagoTextual = "Julio";
+        break;
+        case 8:
+            mesDePagoTextual = "Agosto";
+        break;
+        case 9:
+            mesDePagoTextual = "Septiembre";
+        break;
+        case 10:
+            mesDePagoTextual = "Octubre";
+        break;
+        case 11:
+            mesDePagoTextual = "Noviembre";
+        break;
+        case 12:
+            mesDePagoTextual = "Diciembre";
+        break;
+        default:
+            console.log("Error en el número del mes de pago.");
+        break;
+    }
+    return mesDePagoTextual;
+}
+
+function limpiarFilas()
+{       
+    let i = cantidadPeriodos;
+    while (i > 0)   
+    {
+        document.getElementsByTagName("tr")[i].remove();                   
+        --i;
+    }
+}
